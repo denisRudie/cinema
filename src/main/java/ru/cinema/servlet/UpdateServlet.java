9db9afc79a42788f44834e9101530b03dcc65438
denisRudie <moonstamp1964@gmail.com>
2020-12-04 15:13:00 +0300

    cinema without async

--- src/main/java/ru/cinema/servlet/UpdateServlet.java
+++ src/main/java/ru/cinema/servlet/UpdateServlet.java
@@ -9,37 +9,20 @@ import ru.cinema.model.Seat;
 import ru.cinema.store.PsqlStore;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.io.Writer;
 
-@WebServlet(urlPatterns = "/hall")
-public class HallServlet extends HttpServlet {
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setCharacterEncoding("UTF-8");
-        Collection<Seat> allSeats = PsqlStore.instOf().getAllSeats();
-        String jsonOut = new Gson().toJson(allSeats);
-        try (PrintWriter writer = resp.getWriter()) {
-            writer.write(jsonOut);
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+public class UpdateServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try (BufferedReader read = req.getReader()) {
+        boolean status = true;
+        try (BufferedReader read = req.getReader();
+             Writer writer = resp.getWriter()) {
             StringBuilder fullLine = new StringBuilder();
             String oneLine;
             while ((oneLine = read.readLine()) != null) {
@@ -47,16 +30,16 @@ public class HallServlet extends HttpServlet {
             }
             JSONArray json = (JSONArray) new JSONParser().parse(fullLine.toString());
 
-            HttpSession session = req.getSession();
-            List<Seat> seats = new ArrayList<>();
             for (Object o : json) {
                 JSONObject j = (JSONObject) o;
                 int sId = Integer.parseInt(j.get("id").toString());
-                PsqlStore.instOf().tempSeatOwner(sId, -1);
                 Seat seat = PsqlStore.instOf().getSeatById(sId);
-                seats.add(seat);
+                if (seat.getAccountId() != 0) {
+                    status = false;
+                }
             }
-            session.setAttribute("chosenSeats", seats);
+            writer.write(new Gson().toJson(status));
+            writer.flush();
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
